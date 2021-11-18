16245ac9b677b44bfeb9f95e8d8226753272114a
Pablo Ramos <pablo-362@hotmail.com>
2021-11-18 16:12:24 -0300

    calculator ui

---
+++ Javascript/4 - Calculator/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// const calculator = '<div><div id="display">0</div><div id="keyboard"><ul><li>(</li><li>)</li><li>Del</li><li>AC</li><li>7</li><li>8</li><li>9</li><li>%</li><li>4</li><li>5</li><li>6</li><li>x</li><li>1</li><li>2</li><li>3</li><li>-</li><li>0</li><li>,</li><li>=</li><li>+</li></ul></div></div>'
+// document.getElementById('root').innerHTML=calculator;
+console.log('hola')
+  
